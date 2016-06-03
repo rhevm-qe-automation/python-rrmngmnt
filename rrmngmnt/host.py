5f8e8000a31cd1247581da6bb36da7c5e5d9f88e
Meni Yakove <myakove@redhat.com>
2016-06-03 23:58:47 +0300

    Set FQDN if host created qith FQDN
    Sometime socket.getfqdn(ip) return IP and not the host FQDN so if Host
    was created with FQDN we will use it as FQDN

--- rrmngmnt/host.py
+++ rrmngmnt/host.py
@@ -59,7 +59,9 @@ class Host(Resource):
         :type service_provider: class which implement SystemService interface
         """
         super(Host, self).__init__()
+        self.host_fqdn = None
         if not netaddr.valid_ipv4(ip):
+            self.host_fqdn = ip
             ip = fqdn2ip(ip)
         self.ip = ip
         self.users = list()
@@ -102,7 +104,7 @@ class Host(Resource):
 
     @property
     def fqdn(self):
-        return socket.getfqdn(self.ip)
+        return socket.getfqdn(self.ip) if not self.host_fqdn else self.host_fqdn
 
     def add_power_manager(self, pm_type, **init_params):
         """

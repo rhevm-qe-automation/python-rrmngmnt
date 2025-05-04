2576287c20a4a089ae34785a984389776459609f
Meni Yakove <myakove@gmail.com>
2025-05-04 12:36:25 +0300

    setup.py: WA for https://github.com/pypa/setuptools/issues/4976

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import os
+
 from setuptools import setup
 
-
-os.environ['SKIP_GENERATE_AUTHORS'] = '1'
+os.environ["SKIP_GENERATE_AUTHORS"] = "1"
 
 
 if __name__ == "__main__":
     setup(
-        setup_requires=['pbr'],
-        pbr=True,
+        setup_requires=["pbr"],
+        pbr=False,
     )
